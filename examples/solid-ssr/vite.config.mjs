6c9af8d631bd7181f196a2c8d9da4e8fcd813f06
Rory Hendrickson <35480205+roryhen@users.noreply.github.com>
2025-08-27 15:36:18 -0700

    add solid-js example (#2)

---
+++ examples/solid-ssr/vite.config.mjs
@@ -0,0 +1,16 @@
+import solid from "vite-plugin-solid";
+import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
+
+export default defineConfig({
+  plugins: [solid({ ssr: true }), nitro()],
+  esbuild: {
+    jsx: "preserve",
+    jsxImportSource: "solid-js",
+  },
+  environments: {
+    client: {
+      build: { rollupOptions: { input: "./src/client.tsx" } },
+    },
+  },
+});
