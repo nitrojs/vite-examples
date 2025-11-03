84c81dcaa09912c81040d5fba025e6826c7dfe23
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-11-04 04:26:14 +0900

    feat: add vue-router-ssr example with SSR assets API (#5)
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

---
+++ examples/vue-router-ssr/app/types.d.ts
@@ -0,0 +1,10 @@
+// https://router.vuejs.org/guide/advanced/meta.html#TypeScript
+import "vue-router";
+
+export {};
+
+declare module "vue-router" {
+  interface RouteMeta {
+    assets?: () => Promise<typeof import("*?assets")>;
+  }
+}
